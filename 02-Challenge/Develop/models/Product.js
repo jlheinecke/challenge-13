eb31622339c2b0d6e3b0a5c135fbffeb4d2975cd
jimh <james.l.heinecke@gmail.com>
2023-06-05 18:22:16 -0500

    category and product model updates

--- 02-Challenge/Develop/models/Product.js
+++ 02-Challenge/Develop/models/Product.js
@@ -9,8 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `driver` model, which we set in `Driver.js` as its `modelName` property
+        model: 'category',
+        key: 'id'
+    }
+  }
+},
   {
     sequelize,
     timestamps: false,
