646a94e1f33bf7aa4236175181ff129449eb99c6
jimh <james.l.heinecke@gmail.com>
2023-06-08 00:53:09 -0500

    bug fixes

--- 02-Challenge/Develop/models/index.js
+++ 02-Challenge/Develop/models/index.js
@@ -5,9 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'id',
-});
+Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
--- 02-Challenge/Develop/routes/api/product-routes.js
+++ 02-Challenge/Develop/routes/api/product-routes.js
@@ -32,15 +32,15 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-
-  const newProduct = Product.create(req.body, {
-    where: {
-      product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      tagIds: req.body.tagIds
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
     }
-  })
+  */
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
