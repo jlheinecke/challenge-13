b977e6f7c43ce8339370e256205a96ea3f1820dc
jimh <james.l.heinecke@gmail.com>
2023-06-07 23:25:01 -0500

    formatting updates

--- 02-Challenge/Develop/routes/api/tag-routes.js
+++ 02-Challenge/Develop/routes/api/tag-routes.js
@@ -60,4 +60,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// delete a tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
 module.exports = router;
